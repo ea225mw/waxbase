2bf5b4344bea5486bbb671ebed5248edf32b4a89
Emanuel Andersen <ea225mw@student.lnu.se>
2025-05-27 14:08:57 +0200

    New variable for Digital Ocean.

--- public/js/config/variables.js
+++ public/js/config/variables.js
@@ -1,3 +1,3 @@
 export const baseURLClient = window.location.hostname.includes('localhost')
   ? 'http://localhost:8085/'
-  : 'https://cscloud7-230.lnu.se/waxbase/'
+  : 'https://waxbase-app-5tea3.ondigitalocean.app/'
